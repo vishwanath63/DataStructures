044f8cd36d5d2eabe6ff998a55750cbb131cd109
RAHULSAIRAM <54234991+RAHULSAIRAM@users.noreply.github.com>
2019-10-14 21:45:04 +0530

    Update Driver.java

--- Code/Driver.java
+++ Code/Driver.java
@@ -8,8 +8,8 @@ public class Driver {
 		o1.insert(20);
 		o1.insert(25);
 		o1.insert(35);
-		//o1.insert(10);
-		//o1.insert(5);
+		o1.insert(10);
+		o1.insert(5);
 		o1.inorder();
 		System.out.println("_____________________");
 
